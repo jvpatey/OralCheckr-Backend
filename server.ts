5a9a2e3d6270d4d0e501b2223155d02fe56f4156
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-02-01 13:47:35 -0400

    updated development and production basepath

--- server.ts
+++ server.ts
@@ -13,9 +13,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Read base path from environment variables (defaulting to an empty string)
-const basePath = process.env.BASE_PATH || "";
+// If in development, use an empty string for base path; otherwise, use the BASE_PATH from .env.
+const basePath =
+  process.env.NODE_ENV === "development" ? "" : process.env.BASE_PATH || "";
 app.use(`${basePath}/auth`, authRoutes);
+
+// Log the environment in use
+if (process.env.NODE_ENV === "development") {
+  console.log("Running in development mode.");
+} else {
+  console.log(`Running in production mode. Using base path: ${basePath}`);
+}
 
 connectDB();
 
