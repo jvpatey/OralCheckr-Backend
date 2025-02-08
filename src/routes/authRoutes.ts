745665aee33a17d7dd46a9af1b87a1efbe3575ef
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-02-08 11:42:48 -0400

    updated file tree to include src folder

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { register, login } from "../controllers/authControllers.ts";
+import { register, login } from "../controllers/authControllers";
 
 const router = Router();
 
