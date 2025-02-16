86ae103dbffcd236352086994b5d1ca26838ceda
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-02-16 16:57:56 -0400

    updated comments

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -17,7 +17,7 @@ router.post("/login", login);
 // Guest login
 router.post("/guest-login", guestLogin);
 
-// Logout (Now uses the controller)
+// Logout
 router.post("/logout", logout);
 
 export default router;
