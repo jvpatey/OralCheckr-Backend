fcc1f1050a66c2d51b14cd2624368e3bd5b965fb
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-03-16 12:10:32 -0300

    updating deployment for testing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testPathIgnorePatterns: ["/node_modules/"],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        isolatedModules: true,
+      },
+    ],
+  },
 };
