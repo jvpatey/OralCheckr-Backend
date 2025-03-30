05cfa543a472d1635c289d1bac2308cc7aa0e626
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-03-30 09:50:53 -0300

    updated questionnaire fetch controller and added delete questionnaire

--- src/routes/questionnaireRoutes.ts
+++ src/routes/questionnaireRoutes.ts
@@ -5,17 +5,18 @@ import {
   updateProgress,
   getProgress,
   saveGuestQuestionnaireResponse,
+  deleteQuestionnaireData,
 } from "../controllers/questionnaireControllers";
 import { verifyToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
 /* -- Questionnaire Routes -- */
-router.post("/response", saveResponse); // Save questionnaire response
-router.get("/response", getResponseByUser); // Get questionnaire responses
-router.put("/progress", updateProgress); // Update the questionnaire progress
-router.put("/progress", updateProgress);
-router.get("/progress", getProgress); // Retrieve the questionnaire progress
+router.post("/response", verifyToken, saveResponse); // Save questionnaire response
+router.get("/response", verifyToken, getResponseByUser); // Get questionnaire responses
+router.put("/progress", verifyToken, updateProgress); // Update the questionnaire progress
+router.get("/progress", verifyToken, getProgress); // Retrieve the questionnaire progress
 router.post("/guest", verifyToken, saveGuestQuestionnaireResponse); // Save guest questionnaire responses
+router.delete("/response", verifyToken, deleteQuestionnaireData); // Delete questionnaire data
 
 export default router;
