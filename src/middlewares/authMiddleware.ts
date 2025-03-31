23dabc6e4fe2ea6dabda8866fe50a0a776083bba
Jeffrey Patey <jeffreyvpatey@gmail.com>
2025-03-31 15:54:00 -0300

    added comments to auth middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
 
-/* -- Extend Request to include the user property -- */
+/* -- Middleware to authenticate and verify JWT token from cookies or headers -- */
+
 export interface AuthenticatedRequest extends Request {
   user?: {
     userId: number;
@@ -10,14 +11,17 @@ export interface AuthenticatedRequest extends Request {
   };
 }
 
-/* -- Middleware to authenticate and verify JWT token from cookies or headers -- */
+/* -- Verify the JWT token -- */
 export const verifyToken = (
   req: AuthenticatedRequest,
   res: Response,
   next: NextFunction
 ): void => {
+  // Get the JWT token from the cookies or headers
   const token =
     req.cookies?.accessToken || req.headers.authorization?.split(" ")[1];
+
+  // If the token is provided, verify it
   if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
@@ -32,18 +36,23 @@ export const verifyToken = (
     console.log(`Auth: No token provided for ${req.originalUrl}`);
   }
 
+  // If the token is not provided, send a 401 error
   if (!token) {
     res.status(401).json({ error: "Unauthorized: No token provided" });
     return;
   }
 
+  // If the token is provided, verify it
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
       userId: number;
       role?: string;
     };
 
+    // Set the user in the request
     req.user = { userId: decoded.userId, role: decoded.role };
+
+    // Call the next middleware
     return next();
   } catch (err: any) {
     if (err.name === "TokenExpiredError") {
